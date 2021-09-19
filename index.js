018d6ba238679cbfd509389ecdeae501f6c9f9b8
Aldi William <aldyy1988@gmail.com>
2021-09-19 13:27:38 +0700

    Views Javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+class Card{ 
+
+    createImageComponent(data){
+        data = 'https://i.insider.com/6146891fa060c800184bd84c?width=1200&format=jpeg';
+        let image = `<img class="rounded-t-xl" src='${data}' />`;
+        return image;
+    }
+
+    createHeadingComponent(data){
+        data = 'Title';
+        let heading = `<h1 class="text-center mx-2 my-2">${data}</h1>`;
+        return heading;
+    }
+
+    createParagraphComponent(data){
+        data = 'Paragraph';
+        let paragraph = `<p class="mx-2 my-2">${data}</p>`;
+        return paragraph;
+    }
+
+    createButtonComponent(){
+        let button = `<button class="mx-2 my-2 border rounded-xl bg-yellow-400 py-2 px-2">Read more...</button>`;
+        return button;
+    }
+
+    render(element){
+        let fullComponent = this.createImageComponent + this.createHeadingComponent + this.createParagraphComponent + this.createButtonComponent;
+        element.innerHtml = fullComponent;
+    }
+}
+
+const app = new Card();
+
+app.render(document.getElementById('cardcomponent'));
